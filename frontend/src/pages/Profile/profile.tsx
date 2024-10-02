9b9f2beda45ea82c62b6eb0d5ba363250a90c671
Oshan Gopallawa <oshan8998@gmail.com>
2024-10-02 23:35:58 +0530

    modified
    modified

--- frontend/src/pages/Profile/profile.tsx
+++ frontend/src/pages/Profile/profile.tsx
@@ -347,7 +347,8 @@ export default function Profile() {
             {/* Delete Account Button */}
             <div className="flex justify-end">
               <button
-                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
+                className="bg-red-500 text-white px-5 py-1 rounded-lg hover:bg-red-600"
+              
                 onClick={handleDeleteAccount}
               >
                 Delete Account
