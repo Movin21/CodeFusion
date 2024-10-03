20fc319a1d9f28dcad2e1332d27748a724a2f5a8
Movindu Liyanage <142730691+Movin21@users.noreply.github.com>
2024-10-03 20:19:59 +0530

    Aravinda Update

--- frontend/src/pages/Blogs/Blogs.jsx
+++ frontend/src/pages/Blogs/Blogs.jsx
@@ -1,46 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
 import { Container, Spinner, Text } from "@chakra-ui/react";
 import BlogItem from "./components/Blog-Item";
 import CreateForm from "./components/Create-Form";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchBlogs = async () => {
+  const response = await fetch("http://localhost:5000/blogs/readAllBlogs");
+  if (!response.ok) {
+    throw new Error("Failed to fetch blogs");
+  }
+  return response.json();
+};
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    // Fetch the blog data from the API
-    const fetchBlogs = async () => {
-      try {
-        const response = await fetch('https://localhost:7003/api/blogs');
-        if (response.ok) {
-          const data = await response.json();
-          setBlogs(data); // Store the fetched blogs in the state
-        } else {
-          setError('Failed to fetch blogs');
-        }
-      } catch (error) {
-        setError('Network error');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchBlogs();
-  }, [blogs]);
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["blogs"],
+    queryFn: fetchBlogs,
+  });
 
   return (
     <Container>
-      <CreateForm />
-
-      {loading ? (
-        <Spinner size="xl" />
+      {isLoading ? (
+        <Spinner />
       ) : error ? (
-        <Text color="red.500">{error}</Text>
+        <Text>{error.message}</Text>
       ) : (
-        blogs.map((blog) => (
-          <BlogItem key={blog.id} title={blog.title} content={blog.content} blogId={blog.id}/>
-        ))
+        blogs.map((blog) => <BlogItem key={blog.id} blog={blog} />)
       )}
     </Container>
   );
